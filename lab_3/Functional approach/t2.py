a8810196afc0bbf9cb61a17e64f92ed0df9809fb
olZabi <olzabi@hotmail.com>
2020-11-23 21:16:00 +0200

    Update t2.py

--- lab_3/Functional approach/t2.py
+++ lab_3/Functional approach/t2.py
@@ -3,7 +3,10 @@ import random as r
 N = int(input("choose from 1 to 5? "))
 
 matrix = [[r.randint(0, 30) for i in range(N)] for j in range(N)]
-maximum = [max(i) for i in matrix]
-
 print(matrix)
-print(max(maximum))
+
+max_list = [max(i) for i in matrix]
+max_value = max(max_list)
+maximal_i = max_list.index(max(max_list))
+print(max_list)
+print(maximal_i)
