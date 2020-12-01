508b3b5ddbc51f733864c99fbdb5022b762bcf0d
olZabi <olzabi@hotmail.com>
2020-12-01 17:07:29 +0200

    Update t2.py

--- lab_3/Functional approach/t2.py
+++ lab_3/Functional approach/t2.py
@@ -1,12 +1,22 @@
 import random as r
 
+'''
+была попытка сделать 2 задание с матрицами через генераторы
+'''
+
 N = int(input("choose from 1 to 5? "))
 
 matrix = [[r.randint(0, 30) for i in range(N)] for j in range(N)]
-print(matrix)
-
 max_list = [max(i) for i in matrix]
+
+z = max(max_list)
+# for z in matrix:
+
+
 max_value = max(max_list)
 maximal_i = max_list.index(max(max_list))
+
+
+print(matrix)  # 1+
 print(max_list)
 print(maximal_i)
